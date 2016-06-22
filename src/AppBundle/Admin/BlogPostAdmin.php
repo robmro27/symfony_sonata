d70adf831629728d3a061197b4ebcaec5eb16cb4
robmro27 <robert.mroz1985@gmail.com>
2016-06-22 13:03:45 +0200

    end docs

--- src/AppBundle/Admin/BlogPostAdmin.php
+++ src/AppBundle/Admin/BlogPostAdmin.php
@@ -6,16 +6,67 @@ namespace AppBundle\Admin;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 
 class BlogPostAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        // ... configure $formMapper
+        $formMapper
+            ->with('Content')  
+                ->add('title', 'text')
+                ->add('body', 'textarea')
+            ->end()
+                
+            ->with('Meta data')    
+                ->add('category', 'sonata_type_model', array(
+                    'class' => 'AppBundle\Entity\Category',
+                    'property' => 'name',
+                ))
+            ->end()
+        ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        // ... configure $listMapper
+        $listMapper
+            ->addIdentifier('title')
+            ->add('category.name')
+            ->add('draft')
+            ->add('_action', null, array(
+            'actions' => array(
+                'show' => array(),
+                'edit' => array(),
+                'delete' => array(),
+            )))
+        ;
     }
+    
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('title')
+                       ->add('category', null, array(), 'entity', array(
+                'class'    => 'AppBundle\Entity\Category',
+                'property' => 'name',
+            ));
+    }
+    
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+           ->add('title')
+           ->add('slug')
+           ->add('author')
+       ;
+    }
+    
+    
+    public function toString($object)
+    {
+        return $object instanceof BlogPost
+            ? $object->getTitle()
+            : 'Blog Post'; // shown in the breadcrumb on the create view
+    }
+    
 }
--- src/AppBundle/Admin/CategoryAdmin.php
+++ src/AppBundle/Admin/CategoryAdmin.php
@@ -6,6 +6,7 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use \AppBundle\Entity\Category;
 
 class CategoryAdmin extends AbstractAdmin
 {
@@ -16,11 +17,24 @@ class CategoryAdmin extends AbstractAdmin
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('name');
+        $datagridMapper->add('name', null, array('show_filter' => true));
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('name');
+        $listMapper->addIdentifier('name')
+                   ->add('_action', null, array(
+            'actions' => array(
+                'show' => array(),
+                'edit' => array(),
+                'delete' => array(),
+            )));
+    }
+    
+    public function toString($object)
+    {
+        return $object instanceof Category
+            ? $object->getName()
+            : 'Blog Post'; // shown in the breadcrumb on the create view
     }
 }
